1e8eff73a4fcfa871ccefdfe9a8bf938712a4d71
Mirko Bonadei <mirko.bonadei@gmail.com>
2016-06-06 23:47:55 +0200

    Fixing coding style

--- src/quorum.erl
+++ src/quorum.erl
@@ -1,33 +1,22 @@
 -module(quorum).
 -behaviour(gen_statem).
 
--export([start/2,
-         stop/1
-        ]).
-
--export([init/1,
-         handle_event/4,
-         code_change/4,
-         terminate/3
-        ]).
+-export([start/2, stop/1]).
+-export([init/1, handle_event/4, code_change/4, terminate/3]).
 
 %% RAFT FSM states
--export([follower/3,
-         candidate/3,
-         leader/3
-        ]).
+-export([follower/3, candidate/3, leader/3]).
+
+-callback commit(Command) -> {ok, State} | error.
 
 -record(state, {current_term=0, % (persistent)
                 voted_for=none, % (persistent)
                 log=[], % (persistent) remember that the first index must be 1
-                
                 other_nodes=[],
                 commit_index=0,
                 last_applied=0,
-
-                % only used by the leader
-                next_index=[],
-                match_index=[]
+                next_index=[], % (only for the leader)
+                match_index=[] % (only for the leader)
                }
        ).
 
