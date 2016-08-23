ea4ee8d0715b34f48b1e3caea012120fb1bf1c63
Mirko Bonadei <mirko.bonadei@gmail.com>
2016-08-23 09:29:16 +0200

    Reducing provisioning complexity
    I have decided to use Erlang 19 without kerl to develop quorum. It will be tested on different Erlang releases on Travis CI (at the moment quorum supports only Erlang 19 and above).

--- vm_bootstrap.sh
+++ vm_bootstrap.sh
@@ -1,42 +1,20 @@
 #!/usr/bin/env bash
 
-if [ ! -f /usr/local/bin/kerl ]; then
-    echo "kerl not installed...";
-    cd /usr/local/bin;
-    curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl;
-    chmod a+x kerl;
-    echo "kerl is now installed on the guest machine.";
-
-    echo "installing erlang dependecies...";
-    apt-get install -y build-essential libncurses5-dev openssl libssl-dev fop xsltproc unixodbc-dev;
-    echo "erlang dependencies are now installed";
-else
-    echo "kerl is already installed.";
+if [ ! -f /home/vagrant/erlang-solutions_1.0_all.deb ]; then
+    wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb > /dev/null 2>&1;
+    sudo dpkg -i erlang-solutions_1.0_all.deb > /dev/null 2>&1;
 fi
+
+sudo apt-get -y update && sudo apt-get install -y erlang
 
 if [ ! -f /usr/local/bin/rebar3 ]; then
     echo "rebar3 not installed...";
     cd /usr/local/bin;
-    wget https://s3.amazonaws.com/rebar3/rebar3;
+    wget https://s3.amazonaws.com/rebar3/rebar3 > /dev/null 2>&1;
     chmod +x rebar3;
 
     echo "rebar3 is now installed on the guest machine.";
 else
     echo "rebar3 is already installed.";
 fi
-
-if git --version; then
-    echo "git is already installed.";
-else
-    apt-get install -y git;
-    echo "git is now instaled.";
-fi
-
-if autoconf --version; then
-    echo "autoconf is already installed.";
-else
-    sudo apt-get install -y autoconf
-    echo "autoconf is now instaled.";
-fi
-
     
